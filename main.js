f08eb28c7734e9a7274b6a19645d7a67cb73be24
LeMinhTuong <leminhtuong09122002@gmail.com>
2023-06-20 00:29:35 +0700

    new commit

--- main.js
+++ main.js
@@ -19,10 +19,53 @@ getHeaderCloseIcon.addEventListener("click", hideHeaderMenu);
 
 const getHeader = $(".header");
 const headerOffsetHeightPx = getHeader.offsetHeight;
-const headerOffsetHeightVh = (headerOffsetHeightPx / window.innerHeight) * 100;
-const referenceHeightVh = 100 - headerOffsetHeightVh;
+// const headerOffsetHeightVh = (headerOffsetHeightPx / window.innerHeight) * 100;
+// const referenceHeightVh = 100 - headerOffsetHeightVh;
 
-const getReference = $(".reference");
-getReference.style.marginTop = headerOffsetHeightVh + "vh";
-getReference.style.height = referenceHeightVh + "vh";
+// const getReference = $(".reference");
+// getReference.style.marginTop = headerOffsetHeightVh + "vh";
+// getReference.style.height = referenceHeightVh + "vh";
 
+// const getAbout = $(".contact");
+// getAbout.style.height = referenceHeightVh + "vh";
+
+// Scroll to each section
+document.addEventListener("DOMContentLoaded", function () {
+  var links = document.querySelectorAll("a[href^='#']");
+
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function (event) {
+      event.preventDefault();
+      var targetId = this.getAttribute("href").substring(1);
+      var targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        var targetOffset = targetElement.offsetTop - headerOffsetHeightPx;
+
+        window.scrollTo({
+          top: targetOffset,
+          behavior: "smooth",
+        });
+      }
+    });
+  }
+});
+
+// const getHeaderMenuItems = $$(".header__content a");
+
+// getHeaderMenuItems.forEach((item) => {
+//   item.addEventListener("click", function() {
+//     if (window.innerWidth <= 767) {
+//       hideHeaderMenu();
+//     }
+//   });
+// });
+
+
+
+// const getInfo = $(".info");
+// const infoOffsetHeightPx = getInfo.offsetHeight;
+// const infoOffsetHeightVh = (infoOffsetHeightPx / window.innerHeight) * 100;
+// const footerHeight = referenceHeightVh - infoOffsetHeightVh
+// const getFooter = $('.footer')
+// getFooter.style.height = footerHeight + "vh"
